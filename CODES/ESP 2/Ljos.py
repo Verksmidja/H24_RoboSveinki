9fe03186009d88b65e9a8e4f6b7a793bf53ac1f7
Gazzo00o <145452685+Gazzo00o@users.noreply.github.com>
2024-11-27 16:00:11 +0000

    Create Ljos.py

---
+++ CODES/ESP 2/Ljos.py
@@ -0,0 +1,45 @@
+import machine
+import neopixel
+import uasyncio as asyncio
+
+NUM_PIXELS_1 = 10
+NUM_PIXELS_2 = 10
+PIN_1 = 5
+PIN_2 = 6
+
+np1 = neopixel.NeoPixel(machine.Pin(PIN_1), NUM_PIXELS_1)
+np2 = neopixel.NeoPixel(machine.Pin(PIN_2), NUM_PIXELS_2)
+
+def clear_strip(np):
+    for i in range(len(np)):
+        np[i] = (0, 0, 0)
+    np.write()
+
+async def red_green_switch(np):
+    state = True
+    try:
+        while True:
+            if state:
+                for i in range(len(np)):
+                    np[i] = (255, 0, 0) if i % 2 == 0 else (0, 255, 0)
+            else:
+                for i in range(len(np)):
+                    np[i] = (0, 255, 0) if i % 2 == 0 else (255, 0, 0)
+            np.write()
+            state = not state
+            await asyncio.sleep(0.5)
+    except asyncio.CancelledError:
+        clear_strip(np)
+        raise 
+
+async def ljos():
+    task1 = asyncio.create_task(red_green_switch(np1))
+    task2 = asyncio.create_task(red_green_switch(np2))
+    try:
+        await asyncio.gather(task1, task2)
+    except asyncio.CancelledError:
+        task1.cancel()
+        task2.cancel()
+        await asyncio.gather(task1, task2, return_exceptions=True)
+        raise
+
