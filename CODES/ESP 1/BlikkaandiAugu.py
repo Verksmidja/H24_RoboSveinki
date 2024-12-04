ea1da56962c60e2e32b9f78ef62f9d6c7e27e98c
DaRonXX <122013119+DaRonXX@users.noreply.github.com>
2024-12-04 14:29:03 +0000

    Update BlikkaandiAugu.py

--- CODES/ESP 1/BlikkaandiAugu.py
+++ CODES/ESP 1/BlikkaandiAugu.py
@@ -1,62 +1,32 @@
-import uasyncio as asyncio
-from machine import Pin, PWM, UART
+from machine import Pin, PWM
 
-# Define LED pins for left eye
-left_eye_r = PWM(Pin(9))
-left_eye_g = PWM(Pin(10))
-left_eye_b = PWM(Pin(11))
+# Define PWM frequency
+FREQ = 1000  # 1 kHz frequency
 
-# Define LED pins for right eye
-right_eye_r = PWM(Pin(2))
-right_eye_g = PWM(Pin(42))
-right_eye_b = PWM(Pin(41))
+# RGB LED 1 (Pins 9, 10, 11)
+red1 = PWM(Pin(9))
+green1 = PWM(Pin(10))
+blue1 = PWM(Pin(11))
 
-# Setup PWM frequency for LEDs
-left_eye_r.freq(1000)
-left_eye_g.freq(1000)
-left_eye_b.freq(1000)
-right_eye_r.freq(1000)
-right_eye_g.freq(1000)
-right_eye_b.freq(1000)
+# RGB LED 2 (Pins 3, 42, 41)
+red2 = PWM(Pin(2))
+green2 = PWM(Pin(42))
+blue2 = PWM(Pin(41))
 
-# Initialize UART for MP3-TF-18P module
-uart = UART(1, baudrate=9600, tx=17, rx=16)
+# Set PWM frequency for all pins
+red1.freq(FREQ)
+green1.freq(FREQ)
+blue1.freq(FREQ)
+red2.freq(FREQ)
+green2.freq(FREQ)
+blue2.freq(FREQ)
 
-# Define RGB values for blinking
-def set_eye_color(r, g, b):
-    left_eye_r.duty(r)
-    left_eye_g.duty(g)
-    left_eye_b.duty(b)
-    right_eye_r.duty(r)
-    right_eye_g.duty(g)
-    right_eye_b.duty(b)
+# Function to set color for an RGB LED
+def set_color(led_red, led_green, led_blue, r, g, b):
+    led_red.duty_u16(int(r * 65535 / 255))
+    led_green.duty_u16(int(g * 65535 / 255))
+    led_blue.duty_u16(int(b * 65535 / 255))
 
-# Function to send command to MP3 module to play the specified MP3
-def play_mp3():
-    # Command to play the MP3 file from folder 01, track 001.mp3
-    uart.write(b'\x7E\xFF\x06\x03\x00\x01\x01\xEF')
-
-# Blinking task
-async def blink_eyes():
-    while True:
-        # Set eyes to a color (e.g., red)
-        set_eye_color(512, 0, 0)  # Set red
-        await asyncio.sleep(0.5)
-        
-        # Turn off the LEDs
-        set_eye_color(0, 0, 0)  # Turn off
-        await asyncio.sleep(0.5)
-
-# Music playback task
-async def play_music():
-    while True:
-        play_mp3()  # Send play command to the MP3 module
-        await asyncio.sleep(30)  # Wait for track duration before replaying
-
-# Main async function to run both tasks
-async def main():
-    # Run both tasks concurrently
-    await asyncio.gather(blink_eyes(), play_music())
-
-# Run the main function
-asyncio.run(main())
+# Set both LEDs to white
+set_color(red1, green1, blue1, 255, 255, 255)  # Full brightness for RGB LED 1
+set_color(red2, green2, blue2, 255, 255, 255)  # Full brightness for RGB LED 2
